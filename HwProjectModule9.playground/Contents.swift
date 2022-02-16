eb2beebdd1ccd721f2462a03afee1f60e3455c4d
ruslan <ruslan.regent@yandex.ru>
2022-02-16 12:19:30 +0300

    Добавил оставшиеся методы для управления параметрами

--- HwProjectModule9.playground/Contents.swift
+++ HwProjectModule9.playground/Contents.swift
@@ -28,6 +28,9 @@ enum AutoError: Error {
     case tooColdForAirCondition // Слишком низкая температура для включенного кондиционера
     case tooHighTemp // температуру выше 25 задавать нельзя
     case tooLowTemp // температуру ниже 1 задавать нельзя
+    case incorrectChannel // недопустимый идентификатор канала
+    case incorrectSunroofOpenLevel // некорректно заданный процент открытия крыши
+    case incorrectBlowingFanSpeed // некорректная скорость обдува
 }
 
 /** режим управления */
@@ -74,23 +77,55 @@ func setClimatControlTemp(newValue: Int) throws {
     climatControlTemp = newValue
 }
 
+func getClimatControlTemp() -> Int {
+    return climatControlTemp
+}
+
 // вкл/выкл кондиционера
 func switchСlimatControlAirCondition() {
+    let currentTemp = getClimatControlTemp()
     if !climatControlAirConditionEnabled {
         // надо проверить и отрегулировать температуру
-        if climatControlTemp > 15 {
+        if currentTemp > 15 {
             // хорошо бы через метод setClimatControlTemp
             climatControlTemp = 15
-            print("Температура уменьшена до 15 градусов")
+            print("Температура уменьшена до 15 градусов, чтобы кондицинер включился")
         }
-        if climatControlTemp < 5 {
+        if currentTemp < 5 {
             // хорошо бы через метод setClimatControlTemp
             climatControlTemp = 5
-            print("Температура увеличена до 5 градусов")
+            print("Температура увеличена до 5 градусов, чтобы кондицинер включился")
         }
     }
 
     climatControlAirConditionEnabled = !climatControlAirConditionEnabled
+    
+    print(climatControlAirConditionEnabled ? "Кондиционер включен" : "Кондиционер выключен")
+}
+
+// поменять музыку
+func changeMusic(newChannel: Int) throws {
+    if (newChannel < 1 || newChannel > 6) {
+        throw AutoError.incorrectChannel
+    }
+    
+    musicChannel = newChannel
+}
+
+// открытие крыши
+func setSunroofOpenLevel(newValue: Int) throws {
+    if (newValue < 0 || newValue > 95) {
+        throw AutoError.incorrectSunroofOpenLevel
+    }
+    
+    sunroofOpenLevel = newValue
+}
+
+// скорость обдува
+func setBlowingFanSpeed(newValue: Int) throws {
+    if (newValue < 0 || newValue > 5) {
+        throw AutoError.incorrectBlowingFanSpeed
+    }
 }
 
 // НАЧИНАЕМ ВЫЗЫВАТЬ МЕТОДЫ, проверяем работу программы
@@ -106,7 +141,7 @@ do {
     print("Ошибка! Недопустимо отрицательное значение уровня громкости!")
 }
 
-// пробуем присваивать новые значения громкости музыки
+// пробуем присваивать новые значения температуры в систему климат-контроля
 do {
     try setClimatControlTemp(newValue: -1)
 } catch AutoError.tooHotForAirCondition {
@@ -119,5 +154,28 @@ do {
     print("Ошибка! Нельзя выставить температуру 0 или ниже!")
 }
 
+// включаем кондиционер
+switchСlimatControlAirCondition()
+// и затем выключаем кодниционер
 switchСlimatControlAirCondition()
 
+//меняем радиостанцию на недопустимую
+do {
+    try changeMusic(newChannel: 10)
+} catch AutoError.incorrectChannel {
+    print("Ошибка! Вы пытаетесь выбрать ненастроенный канал!")
+}
+
+//меняем радиостанцию на недопустимую
+do {
+    try setSunroofOpenLevel(newValue: 100)
+} catch AutoError.incorrectSunroofOpenLevel {
+    print("Ошибка! Процент открытия должен быть не менее 0 и не более 95!")
+}
+
+//меняем радиостанцию на недопустимую
+do {
+    try setBlowingFanSpeed(newValue: -1)
+} catch AutoError.incorrectBlowingFanSpeed {
+    print("Ошибка! Скорость обдува должна быть от 0 до 5!")
+}
