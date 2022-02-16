7f19658da969a677dacb85fe4c6dcf7c0bc141f5
ruslan <ruslan.regent@yandex.ru>
2022-02-16 08:24:18 +0300

    Initial Commit

---
+++ HwProjectModule9.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+/** характеристики */
+let controlMode = "manual" // режим управления manual или autopilot
+let weight = 2108 // кг
+let length = 4976 // мм
+let width = 1963 // мм
+let height = 1435 // мм
+let wheelbase = 2959 // мм
+let clearance = 154.9 // мм
+let trunkVolume = 900 // л
+
+/** nтекущие параметры */
+var musicVolume = 0 // громкость 0..10
+var musicChannel = 1 // канал 1..6
+
+var sunroofOpenLevel = 50 // процент открытия крыши
+var blowingFanSpeed = 2 // 0..5 скорость обдува
+
+
