b9762887f9aa7d1f6b90b6e2eb1534cb65b190a8
ruslan <ruslan.regent@yandex.ru>
2022-02-16 10:14:46 +0300

    Добавляю обработку ошибок

--- HwProjectModule9.playground/Contents.swift
+++ HwProjectModule9.playground/Contents.swift
@@ -10,11 +10,36 @@ let wheelbase = 2959 // мм
 let clearance = 154.9 // мм
 let trunkVolume = 900 // л
 
-/** nтекущие параметры */
+/** начальные параметры */
 var musicVolume = 0 // громкость 0..10
 var musicChannel = 1 // канал 1..6
+var sunroofOpenLevel = 0 // процент открытия крыши
+var blowingFanSpeed = 0 // 0..5 скорость обдува
+var climatControlTemp = 20 // температура градусов
+var climatControlAirConditionEnabled = false // включение кондиционера
 
-var sunroofOpenLevel = 50 // процент открытия крыши
-var blowingFanSpeed = 2 // 0..5 скорость обдува
+/** закладываем набор ошибок */
+enum AutoError: Error {
+    case tooLoudMusic // Выше максимальной громкости
+    case tooQuietMusic // Ниже нуля
+}
 
+func autoDrive() throws {
+    if musicVolume > 10 {
+        throw AutoError.tooLoudMusic
+    }
+    if musicVolume < 0 {
+        throw AutoError.tooQuietMusic
+    }
+}
+    
+// пробуем присваивать новые значения
+musicVolume = 11
 
+do {
+    try autoDrive()
+} catch AutoError.tooLoudMusic {
+    print("Музыкальная система не предусмотрена для более громкого звука!")
+} catch AutoError.tooQuietMusic {
+    print("Недопустимо отрицательное значение громкости!")
+}
